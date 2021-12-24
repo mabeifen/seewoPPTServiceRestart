5e7a2ba930d90471b14e10c17484e3cf934cdd4f
misakano7545 <misakano7545@protonmail.com>
2021-12-24 18:18:30 +0000

    Upload files by shell

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# seewoPPTServiceRestart
+# 希沃PPT小工具重启助手
+
 本程序为学校使用希沃一体机的老师们不再受到希沃PPTService异常退出的困扰
+
+## 效果演示
+
+本人使用的是Surface作为程序的开发环境，未在一体机上做过测试。理论上应该是可以成功调用并重启的
+
+![image.png](https://s2.loli.net/2021/12/25/mtL5vejP1IRqzZ3.png)
+
+## 系统要求
+
+因本程序是以Python 3.10.1为基础进行封装使用的，故使用Windows 7的一体机可能无法使用，请见谅！
+
+## 赞助我们
+
+![afdian-MisakaNo.jpg](https://s2.loli.net/2021/12/25/SimocqwhVg89NQJ.jpg)
---
+++ main.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+import os
+import winreg
+import subprocess
+
+# 通过读取本机注册表，获取希沃PPT小工具的安装位置
+string = r'SOFTWARE\WOW6432Node\Seewo\PPTService'
+handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, string, 0, (winreg.KEY_WOW64_64KEY + winreg.KEY_READ))
+location, _type = winreg.QueryValueEx(handle, "ExePath")
+
+# 调用系统命令，重启希沃PPT小工具
+os.system('taskkill /f /im PPTService.exe')
+subprocess.Popen(location)
